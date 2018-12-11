3d43b079a5cb7e984f9b2b96d9cd63fe6e00bd76
froliceric0914 <wzhao.eric@gmail.com>
2018-12-11 21:45:11 +0000

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method:current_user
+  #broadcast so this method is available to all the viewings
 
   def authorize
     redirect_to '/login' unless current_user
